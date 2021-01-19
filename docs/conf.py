cb3ab608ba9dd488bdf281aea83eb40330fc6922
Erik Körner <koerner@informatik.uni-leipzig.de>
2021-01-19 17:37:07 +0100

    Bump version: 0.4.0 → 0.4.1
    Signed-off-by: Erik Körner <koerner@informatik.uni-leipzig.de>

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "Heritrix3 Client"
 year = "2021"
 author = "Erik Körner"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.4.0"
+version = release = "0.4.1"
 
 pygments_style = "trac"
 templates_path = ["."]
